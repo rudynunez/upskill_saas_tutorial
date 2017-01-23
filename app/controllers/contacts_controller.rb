83c76acd46b7c8464957b99588668b565748821c
Rudy Nunez <rudy.nunez@gmail.com>
2017-01-23 01:00:59 +0000

    Added form validations to model

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,7 +1,8 @@
 class ContactsController < ApplicationController
-def new
-  @contact = Contact.new
-end
+  def new
+    @contact = Contact.new
+  end
+  
 def create
   @contact = Contact.new(contact_params)
   if @contact.save
@@ -10,6 +11,7 @@ def create
      redirect_to new_contact_path, notice: "Error occured."
   end
 end
+
 private
   def contact_params
      params.require(:contact).permit(:name, :email, :comments)
