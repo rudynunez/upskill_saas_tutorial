fdde30b6cea10f977ffae4fd510a9fd0e0876b63
Rudy Nunez <rudy.nunez@gmail.com>
2017-02-18 03:05:59 +0000

    added mailer settings to environment file

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smpt_settings = {
+    :port           => ENV['MAILGUN_SMPT_PORT'],
+    :address        => ENV['MAILGUN_SMPT_SERVER'],
+    :user_name      => ENV['MAILGUN_SMPT_LOGIN'],
+    :password       => ENV['MAILGUN_SMPT_PASSWORD'],
+    :domain         => 'fast-hamlet-99855.herokuapp.com',
+    :authentication => :plain,
+}
+ActionMailer::Base.delivery_method = :smpt
